98e9d111544e7d329363eec30cfe1f5631f25cfd
austen.shelton <austen.shelton@gmail.com>
2021-10-22 09:18:20 -0400

    funtionality

--- src/App.js
+++ src/App.js
@@ -72,121 +72,126 @@ export default class App extends Component {
     }
 
     itemsLeft() {
-        let filteredArr = this.state.itemArr.filter(todo => !todo.completed && !todo.deleted)
-        if (filteredArr.length > 1) {
-            return filteredArr.length + " items to do";
-        } else if (filteredArr.length > 0) {
-            return filteredArr.length + " item to do";
+        let activeArr = this.state.itemArr.filter(todo => !todo.deleted);
+        let filteredArr = this.state.itemArr.filter(todo => !todo.completed && !todo.deleted);
+        let completedArr = this.state.itemArr.filter(todo => todo.completed && !todo.deleted);
+        if (this.state.filterBy === "all") {
+            return activeArr.length + " of " + activeArr.length;
+        } else if (this.state.filterBy === "active") {
+            return filteredArr.length + " of " + activeArr.length;
+        } else if (this.state.filterBy === "completed") {
+            return completedArr.length + " of " + activeArr.length;
         } else {
             return ""
         }
     }
 
-    handleKeyPress(e) {
-        if (e.key === 'Enter') {
-            this.createItem()
+
+handleKeyPress(e) {
+    if (e.key === 'Enter') {
+        this.createItem()
+    }
+}
+
+handleChange(event) {
+    this.setState({ currentToDo: event.target.value });
+}
+
+complete(id) {
+    let newArr = this.state.itemArr.map(todo => {
+        if (todo.id === id) {
+            todo.completed = !todo.completed
         }
-    }
+        return todo;
+    });
+    this.setState({ itemArr: newArr })
+}
+delete (id) {
+    let newArr = this.state.itemArr.map(todo => {
+        if (todo.id === id) {
+            todo.deleted = !todo.deleted
+        }
+        return todo;
+    });
+    this.setState({ itemArr: newArr })
+}
 
-    handleChange(event) {
-        this.setState({ currentToDo: event.target.value });
-    }
+restoreActive() {
+    let newArr = this.state.itemArr.filter(todo => todo.completed)
+    this.setState({ newArr: newArr.map(todo => todo.completed = false) })
+}
 
-    complete(id) {
-        let newArr = this.state.itemArr.map(todo => {
-            if (todo.id === id) {
-                todo.completed = !todo.completed
-            }
-            return todo;
-        });
-        this.setState({ itemArr: newArr })
-    }
-    delete(id) {
-        let newArr = this.state.itemArr.map(todo => {
-            if (todo.id === id) {
-                todo.deleted = !todo.deleted
-            }
-            return todo;
-        });
-        this.setState({ itemArr: newArr })
-    }
+deleteAll() {
+    let newArr = this.state.itemArr.filter(todo => todo.completed)
+    this.setState({ newArr: newArr.map(todo => todo.deleted = true) })
+}
 
-    restoreActive() {
-        let newArr = this.state.itemArr.filter(todo => todo.completed)
-        this.setState({ newArr: newArr.map(todo => todo.completed = false) })
-    }
+completeAll() {
+    let newArr = this.state.itemArr.filter(todo => !todo.completed)
+    this.setState({ newArr: newArr.map(todo => todo.completed = true) })
+}
 
-    deleteAll() {
-        let newArr = this.state.itemArr.filter(todo => todo.completed)
-        this.setState({ newArr: newArr.map(todo => todo.deleted = true) })
-    }
-
-    completeAll() {
-        let newArr = this.state.itemArr.filter(todo => !todo.completed)
-        this.setState({ newArr: newArr.map(todo => todo.completed = true) })
-    }
-
-    render() {
-        let tempList = this.read();
-        return (
-            <container className="m-2">
-                <div className="row m-2">
-                    <div className="col text-center display-2 text-primary fw-bold">To Do List</div>
+render() {
+    let tempList = this.read();
+    return (
+        <container className="m-2">
+            <div className="row m-2">
+                <div className="col text-center display-2 text-primary fw-bold">To Do List</div>
+            </div>
+            <div className="row m-1">
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        className="form-control col-8"
+                        value={this.state.currentToDo}
+                        placeholder="Add your To Do List Item"
+                        onChange={this.handleChange}
+                        onKeyPress={this.handleKeyPress}
+                    />
+                    <button type="button" className="btn btn-success btn-lg text-primary" onClick={this.createItem}>
+                        <i class="bi bi-plus-circle-fill"></i>
+                    </button>
                 </div>
-                <div className="row m-1">
-                    <div className="input-group mb-3">
-                        <input
-                            type="text"
-                            className="form-control col-8"
-                            value={this.state.currentToDo}
-                            placeholder="Add your To Do List Item"
-                            onChange={this.handleChange}
-                            onKeyPress={this.handleKeyPress}
-                        />
-                        <button type="button" className="btn btn-success btn-lg text-primary" onClick={this.createItem}>
-                            <i class="bi bi-plus-circle-fill"></i>
-                        </button>
-                    </div>
+            </div>
+            {tempList}
+            <div className="row d-flex align-items-center">
+                <div className="col text-center h4 text-primary fw-bold">
+                    {this.itemsLeft()}
                 </div>
-                {tempList}
-                <div className="row d-flex align-items-center">
-                    <div className="col text-center h4 text-primary fw-bold">
-                        {this.itemsLeft()}
-                    </div>
+            </div>
+            <div className="row m-1">
+                <div className="btn-group d-flex justify content-center">
+                    <button
+                        className="btn btn-success p-1 text-primary fw-bold"
+                        onClick={() => this.setState({ filterBy: "all" })}
+                    >All</button>
+                    <button
+                        className="btn btn-success p-1 text-primary fw-bold"
+                        onClick={() => this.setState({ filterBy: "active" })}
+                    >Active</button>
+                    <button
+                        className="btn btn-success p-1 text-primary fw-bold"
+                        onClick={() => this.setState({ filterBy: "completed" })}
+                    >Completed</button>
                 </div>
-                <div className="row m-1">
-                    <div className="btn-group d-flex justify content-center">
-                        <button
-                            className="btn btn-success p-1 text-primary fw-bold"
-                            onClick={() => this.setState({ filterBy: "all" })}
-                        >All</button>
-                        <button
-                            className="btn btn-success p-1 text-primary fw-bold"
-                            onClick={() => this.setState({ filterBy: "active" })}
-                        >Active</button>
-                        <button
-                            className="btn btn-success p-1 text-primary fw-bold"
-                            onClick={() => this.setState({ filterBy: "completed" })}
-                        >Completed</button>
-                    </div>
+            </div>
+            <div classname="row m-1">
+                <div className="btn-group d-flex justify content-center">
+                    <button
+                        className="btn btn-success p-1 text-primary fw-bold"
+                        onClick={this.restoreActive}
+                    >Restore Completed</button>
+                    <button
+                        className="btn btn-success p-1 text-primary fw-bold"
+                        onClick={this.completeAll}
+                    >Complete All</button>
+                    <button
+                        className="btn btn-success p-1 text-primary fw-bold"
+                        onClick={this.deleteAll}
+                    >Delete Comnpleted</button>
                 </div>
-                <div classname="row m-1">
-                    <div className="btn-group d-flex justify content-center">
-                        <button
-                            className="btn btn-success p-1 text-primary fw-bold"
-                            onClick={this.restoreActive}
-                        >Restore Completed</button>
-                        <button
-                            className="btn btn-success p-1 text-primary fw-bold"
-                            onClick={this.completeAll}
-                        >Complete All</button>
-                        <button
-                            className="btn btn-success p-1 text-primary fw-bold"
-                            onClick={this.deleteAll}
-                        >Delete Comnpleted</button>
-                    </div>
-                </div>
-            </container>
-        );
-    }
+            </div>
+        </container>
+    );
+}
 }
